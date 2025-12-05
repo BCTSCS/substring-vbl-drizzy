35969373cfd42106aebc254c86992f283054cd22
Jayden Argudo <jayarg26@bergen.org>
2025-12-05 15:24:02 -0500

    12/5 JaydenA class

---
+++ JaydenA.java
@@ -0,0 +1,23 @@
+public class JaydenA {
+    private String month;
+    private int day;
+    private String gift;
+    public JaydenA(String month, int day, String gift) {
+        this.month = month;
+        this.day = day;
+        this.gift = gift;
+    }
+    public String getGift() {
+        return gift;
+    }
+    public void setGift(String gift) {
+        this.gift = gift;
+    }
+    public String toString() {
+        return "JaydenA: " + this.month + this.day + " - " + this.gift;
+    }
+    public static void main(String[] args) {
+        JaydenA jayden = new JaydenA("April ", 3, "bike");
+        System.out.println(jayden.toString());
+    }
+}
