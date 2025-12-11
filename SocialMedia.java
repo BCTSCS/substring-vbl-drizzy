a2650ba82949af74d0b104d6082259f0f5aefb1d
Jayden Argudo <jayarg26@bergen.org>
2025-12-11 14:17:54 -0500

    12/11 - Finshed social media

--- SocialMedia.java
+++ SocialMedia.java
@@ -9,12 +9,30 @@ public class SocialMedia {
 
     public int postsWithLength(int minLength) {
         int count = 0;
+        for (String post : posts) {
+            if (post.length() >= minLength) {
+                count++;
+            }
+        }
         return count;
     }
 
     public ArrayList<String> postsWithHashtag(String hashtag) {
         ArrayList<String> result = new ArrayList<>();
+        // .contains
+        for (String post : posts) {
+            if (post.contains(hashtag)) {
+                result.add(post);
+            }
+        }
         return result;
     }
 
+    public static void main(String[] args) {
+        SocialMedia jayden = new SocialMedia("sample1.txt");
+        System.out.println(jayden.postsWithHashtag("Java"));
+        System.out.println(jayden.postsWithLength(5));
+        // jayden.postsWithHashtag("Java").stream().forEach(System.out::println); // Advanced java
+        // SocialMedia unknown = new SocialMedia("sample2.txt");
+    }
 }
